0a418ad5daab1b710c7694a2b35db8d7ef2d3128
Jorda <Jorda@JORDAN-CARO>
2024-01-29 14:35:20 +0100

    Add project files.

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace ConsoleCalculator
+{
+    internal class Program
+    {
+        static double number1 = 0;
+        static double number2 = 0;
+        static void Main(string[] args)
+        {
+            home();
+        }
+        static void home()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a first number");
+                if (double.TryParse(Console.ReadLine(), out number1))
+                {
+                    Console.WriteLine("Enter a second number");
+                    if (double.TryParse(Console.ReadLine(), out number2))
+                    {
+                        Result();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid number");
+                }
+            }
+        }
+
+        static void Result()
+        {
+            Console.WriteLine("Enter an operation: +, -, *, /");
+            string operation = Console.ReadLine();
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = Sum();
+                    break;
+                case "-":
+                    result = Sub();
+                    break;
+                case "*":
+                    result = Mult();
+                    break;
+                case "/":
+                    result = Div();
+                    break;
+                default: Console.WriteLine("Not valid operation."); break;
+            }
+            Console.WriteLine($"Result: {number1} {operation} {number2} = {result}");
+        }
+
+        static double Sum()
+        {
+            return number1 + number2;
+        }
+        static double Sub()
+        {
+            return number1 - number2;
+        }
+        static double Mult()
+        {
+            return number1 * number2;
+        }
+        static double Div()
+        {
+            if (number2 == 0)
+            {
+                Console.WriteLine("cannot be divided by 0");
+                return 0;
+            }
+            return number1 / number2;
+        }
+    }
+}
---
+++ PersonalProjects/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MeterToFoot
+{
+    class Program
+    {
+        public static double amountInput = 0;
+        static void Main(string[] args)
+        {
+            home();
+        }
+
+        public static void home()
+        {
+            while (true)
+            {
+                Console.WriteLine("Length you want to convert from meters to foot:");
+                AmountInFoots();
+                Console.WriteLine("Do you want to convert another length? (y/n)");
+                string response = Console.ReadLine(); 
+                if (response.ToLower() != "y")
+                {
+                    Console.WriteLine("Thanks for using me!");
+                    break; // Exiting to the loop
+                }
+            }
+        }
+        public static void AmountInFoots()
+        {
+            double AmountInFoots = 0;
+            if (double.TryParse(Console.ReadLine(), out amountInput))
+            {
+                AmountInFoots = (amountInput * 3.28084);
+                Console.WriteLine($"Your {amountInput} meter are {AmountInFoots} foot n.n");
+            } else
+            {
+                Console.WriteLine("Enter a valid number");
+            }
+        }
+    }
+}
